6a8b021b6244ca0c62b20eb76f6210f4d093ca66
ashley <126aperezmorales@frhsd.com>
2025-01-14 14:02:44 -0500

    3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-
-function changeCandleColor(color) {
-    const candle = document.getElementById('candle');
-    candle.style.backgroundColor = color;
-}
-
-
+$("#swatch1").hover(function(){
+    $("#picture").attr("src", "img/file.png");
+  });
+$("#swatch2").hover(function(){
+    $("#picture").attr("src", "img/hi.png");
+  });
+$("#swatch3").hover(function(){
+    $("#picture").attr("src", "img/haha.png");
+  });
+$("#swatch4").hover(function(){
+    $("#picture").attr("src","img/cook.png" );
+  });
+$("#swatch5").hover(function(){
+    $("#picture").attr("src", "img/tj.png");
+  });
